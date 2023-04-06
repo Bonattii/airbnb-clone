e29a51d51325ad392405fbbe4c72181b06c556e0
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-04-06 16:17:48 -0300

    Create Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
+import ToasterProvider from './providers/ToasterProvider';
+import RegisterModal from './components/modals/RegisterModal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -22,6 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
 
