343896d3e337c12ef3b68058eb1a222d9cf6ecee
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-04-10 10:37:31 -0300

    Login functionality

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { User } from '@prisma/client';
+
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 import Container from '../Container';
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -19,7 +25,7 @@ const Navbar = () => {
           >
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
+import LoginModal from './components/modals/LoginModal';
 import ToasterProvider from './providers/ToasterProvider';
 import RegisterModal from './components/modals/RegisterModal';
 
@@ -15,18 +17,21 @@ const font = Nunito({
   subsets: ['latin']
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
 
         {children}
